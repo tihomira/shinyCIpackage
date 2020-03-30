9c8504fbf956a3c8b03b3d89d26f15ffd2611be0
Tihomira Nikolova <tiholova8@gmail.com>
2020-03-30 16:55:20 +0200

    change title

--- dscore/app.R
+++ dscore/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("Future Dscore app"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(

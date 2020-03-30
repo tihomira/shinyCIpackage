9877d9fef011e6f388f0f0f823393290355c328d
Tihomira Nikolova <tiholova8@gmail.com>
2020-03-30 17:14:38 +0200

    ssh works

--- dscore/app.R
+++ dscore/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Dscore appp"),
+   titlePanel("Dscore app"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
